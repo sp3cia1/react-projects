750969fdafbb3c67b5130df9d7a6778971d0aaa0
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-01-11 16:14:02 +0530

    fixed build issue

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,6 +1,6 @@
 import Tabs from './components/Tabs'
 import TodoInput from './components/TodoInput'
-import TodoList from './components/ToDoList'
+import TodoList from './components/TodoList'
 import Header from './components/Header'
 import { useState, useEffect } from 'react'
 
